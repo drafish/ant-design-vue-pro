d9dc4fed6cc3fe565f1fcfdd243936a3372a7459
drafish <xwlyy1991@163.com>
2019-07-18 11:09:23 +0800

    封装Result组件，完善RegisterResult页面

---
+++ src/components/Result/index.vue
@@ -0,0 +1,96 @@
+<script>
+import { Icon } from "ant-design-vue";
+
+export default {
+  functional: true,
+  render(h, ctx) {
+    const {
+      className,
+      type,
+      title,
+      description,
+      extra,
+      actions,
+      ...restProps
+    } = ctx.props;
+
+    const iconMap = {
+      error: <Icon class="error" type="close-circle" theme="filled" />,
+      success: <Icon class="success" type="check-circle" theme="filled" />
+    };
+
+    const props = { props: { ...restProps } };
+
+    return (
+      <div class={`result ${className}`} {...props}>
+        <div class="icon">{iconMap[type]}</div>
+        <div class="title">{title}</div>
+        {description && <div class="description">{description}</div>}
+        {extra && <div class="extra">{extra}</div>}
+        {actions && <div class="actions">{actions}</div>}
+      </div>
+    );
+  }
+};
+</script>
+
+<style lang="less" scoped>
+@import "~ant-design-vue/lib/style/themes/default.less";
+
+.result {
+  width: 72%;
+  margin: 0 auto;
+  text-align: center;
+  @media screen and (max-width: @screen-xs) {
+    width: 100%;
+  }
+
+  .icon {
+    margin-bottom: 24px;
+    font-size: 72px;
+    line-height: 72px;
+
+    & > .success {
+      color: @success-color;
+    }
+
+    & > .error {
+      color: @error-color;
+    }
+  }
+
+  .title {
+    margin-bottom: 16px;
+    color: @heading-color;
+    font-weight: 500;
+    font-size: 24px;
+    line-height: 32px;
+  }
+
+  .description {
+    margin-bottom: 24px;
+    color: @text-color-secondary;
+    font-size: 14px;
+    line-height: 22px;
+  }
+
+  .extra {
+    padding: 24px 40px;
+    text-align: left;
+    background: #fafafa;
+    border-radius: @border-radius-sm;
+
+    @media screen and (max-width: @screen-xs) {
+      padding: 18px 20px;
+    }
+  }
+
+  .actions {
+    margin-top: 32px;
+
+    button:not(:last-child) {
+      margin-right: 8px;
+    }
+  }
+}
+</style>
--- src/views/User/RegisterResult.vue
+++ src/views/User/RegisterResult.vue
@@ -1,33 +1,36 @@
-<template>
-  <div class="registerResult">
-    <div>你的账户：{{ account }} 注册成功</div>
-    <div>
-      激活邮件已发送到你的邮箱中，邮件有效期为24小时。请及时登录邮箱，点击邮件中的链接激活帐户。
-    </div>
-    <div class="actions">
-      <a href="">
-        <a-button size="large" type="primary">
-          查看邮箱
-        </a-button>
-      </a>
-      <router-link to="/">
-        <a-button size="large">
-          返回首页
-        </a-button>
-      </router-link>
-    </div>
-  </div>
-</template>
-
 <script>
+import Result from "@/components/Result";
+
 export default {
-  data() {
-    return {
-      account: ""
-    };
-  },
-  created() {
-    this.account = this.$route.params.account;
+  render() {
+    const account = this.$route.params.account;
+    const actions = (
+      <div class="actions">
+        <a href="">
+          <a-button size="large" type="primary">
+            查看邮箱
+          </a-button>
+        </a>
+        <router-link to="/">
+          <a-button size="large">返回首页</a-button>
+        </router-link>
+      </div>
+    );
+
+    return (
+      <Result
+        className="registerResult"
+        type="success"
+        title={
+          <div class="title">
+            你的账户：{account || "AntDesign@example.com"} 注册成功
+          </div>
+        }
+        description="激活邮件已发送到你的邮箱中，邮件有效期为24小时。请及时登录邮箱，点击邮件中的链接激活帐户。"
+        actions={actions}
+        style={{ marginTop: 56 }}
+      />
+    );
   }
 };
 </script>
